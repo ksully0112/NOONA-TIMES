b4d99f0c344370a0965b03e2795943cf73bbea47
Jihyun Kim <ksully0112@gmail.com>
2024-07-09 23:12:50 +0800

    first commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY=`557ab4c2cf244be5a7d3bd6382bcb22a`;
 let news = []
 const getLatesNews = async () => {
     const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        `https://startling-sundae-91d173.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
     );
     const response = await fetch(url);
     const data = await response.json();
