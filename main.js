6d936d9127780ccd0dfdeee6a741e5c0061cb390
Jihyun Kim <ksully0112@gmail.com>
2024-07-09 22:50:22 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const API_KEY=`557ab4c2cf244be5a7d3bd6382bcb22a`;
+let news = []
+const getLatesNews = async () => {
+    const url = new URL(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    );
+    const response = await fetch(url);
+    const data = await response.json();
+    news = data.articles;
+    console.log("ddddd", news);
+};
+
+getLatesNews();
