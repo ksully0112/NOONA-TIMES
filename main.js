a111a489a444a0f890251c4641c43d442b79fb26
Jihyun Kim <ksully0112@gmail.com>
2024-07-12 07:05:15 +0800

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const API_KEY = `557ab4c2cf244be5a7d3bd6382bcb22a`;
 let newsList = [];
+const menus = document.querySelectorAll(".menus button")
+menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)))
 
 const getLatesNews = async () => {
     const url = new URL(
@@ -11,6 +13,17 @@ const getLatesNews = async () => {
     render();
     console.log("News List:", newsList);
 };
+
+const getNewsByCategory = async (event) => {
+    const category = event.target.textContent.toLowerCase()
+    console.log("category, category")
+    const url = new URL (`https://startling-sundae-91d173.netlify.app/top-headlines?country=us&category=${category}&apikey=${API_KEY}`)
+    const response = await fetch(url)
+    const data = await esponse.json()
+    console.log("Ddd", data)
+    newsList = data.articles
+    render()
+}
 
 const render = () => {
     const newsHTML = newsList.map(news => {
@@ -46,3 +59,4 @@ const render = () => {
 };
 
 getLatesNews();
+
